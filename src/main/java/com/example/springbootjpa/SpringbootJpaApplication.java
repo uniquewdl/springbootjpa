b55f166780720433192b4e9465e0c8958bd1e8a8
uniquewdl <1581981226@qq.com>
2019-06-18 08:52:26 +0800

    去除密码

--- src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class SpringbootJpaApplication {
-    public static void main(String[] args) {
+    public static void main(String[]  args) {
         SpringApplication.run(SpringbootJpaApplication.class, args);
     }
 }
